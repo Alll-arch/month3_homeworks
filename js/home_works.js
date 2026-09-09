124747b48e3ccea5c271733963d4932c434c05df
All <dlyabunta@gmail.com>
2026-09-09 14:46:39 +0600

    homework2

--- js/home_works.js
+++ js/home_works.js
@@ -18,18 +18,114 @@ gmailButton.onclick = () => {
 const childBlock = document.querySelector('.child_block');
 
 let positionX = 0;
+let positionY = 0;
+let direction = 'right';
+
 
 const moveBlock = () => {
  
     const parentWidth = document.querySelector('.parent_block').clientWidth;
+    const parentHeight = document.querySelector('.parent_block').clientHeight;
     const childWidth = childBlock.clientWidth;
-    const maxOffset = parentWidth - childWidth;
+    const childHeight = childBlock.clientHeight;
 
-    if (positionX < maxOffset) {
-        positionX++; 
-        childBlock.style.left = `${positionX}px`; 
+    const maxOffsetWidth = parentWidth - childWidth;
+    const maxOffsetHeight = parentHeight - childHeight;
+
+    if (direction === 'right') {
+    if (positionX < maxOffsetWidth) {
+      positionX++;
+    } else {
+      direction = 'down'; 
+    }
+  } else if (direction === 'down') {
+    if (positionY < maxOffsetHeight) {
+      positionY++;
+    } else {
+      direction = 'left'; 
+    }
+  } else if (direction === 'left') {
+    if (positionX > 0) {
+      positionX--;
+    } else {
+      direction = 'up'; 
+    }
+  } else if (direction === 'up') {
+    if (positionY > 0) {
+      positionY--;
+    } else {
+      direction = 'right'; 
+    }
+  }
+
+  childBlock.style.left = `${positionX}px`;
+  childBlock.style.top = `${positionY}px`;
+
         requestAnimationFrame(moveBlock); 
+    };
+
+moveBlock();
+
+
+const secondsDisplay = document.querySelector('#seconds');
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
+
+let timerId = null;
+let currentSeconds = 0;
+
+const updateDisplay = () => {
+    secondsDisplay.textContent = currentSeconds;
+};
+
+startButton.onclick = () => {
+    if (timerId === null) {
+        timerId = setInterval(() => {
+            currentSeconds++;
+            updateDisplay();
+        }, 1000);
     }
 };
 
-moveBlock();
+stopButton.onclick = () => {
+    clearInterval(timerId);
+    timerId = null; 
+};
+
+resetButton.onclick = () => {
+    clearInterval(timerId);
+    timerId = null;
+    currentSeconds = 0;
+    updateDisplay();
+};
+
+
+const firstPromise = new Promise((resolve, reject) => {
+    const isSuccess = true; 
+    if (isSuccess) {
+        resolve("Первый промис успешно выполнился!");
+    } else {
+        reject("Первый промис завершился ошибкой!");
+    }
+});
+
+firstPromise
+    .then(
+        (successData) => {
+            console.log("Резолв 1:", successData);
+            return "Данные переданы во второй промис";
+        },
+        (errorData) => {
+            console.log("Реджект 1:", errorData);
+            return "Данные после ошибки переданы во второй промис";
+        }
+    )
+    .then(
+        (nextSuccessData) => {
+            console.log("Резолв 2:", nextSuccessData);
+        },
+        (nextErrorData) => {
+            console.log("Реджект 2:", nextErrorData);
+        }
+    );
